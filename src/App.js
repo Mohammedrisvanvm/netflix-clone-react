0cf0d5b0d1d42fc9865f8220cc077af60c39d4fe
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2023-05-23 19:53:27 +0530

    added banner style and btns

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import React from "react";
 import NavBar from "./components/navbar/navbar";
 import './App.css'
+import Banner from "./components/banner/Banner";
 
 function App() {
   return (
     <div className="App" >
      <NavBar/>
+     <Banner/>
     </div>
   );
 }
