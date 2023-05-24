968aa84726be511cb8cf59a3371436eccc069557
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2023-05-24 10:08:36 +0530

    posters created

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from "react";
 import NavBar from "./components/navbar/navbar";
 import './App.css'
 import Banner from "./components/banner/Banner";
+import MainPost from "./components/MainPost/MainPost";
 
 function App() {
   return (
     <div className="App" >
      <NavBar/>
      <Banner/>
+     <MainPost/>
     </div>
   );
 }
